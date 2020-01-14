12000c7730868a1bfda93004169fd9b7f9c7f157
MR.Wang <493258141@qq.com>
2020-01-14 11:58:06 +0800

    Revert "update"

--- src/renderer/components/Task/Index.vue
+++ src/renderer/components/Task/Index.vue
@@ -1,18 +1,11 @@
 <template>
-  <el-container class="content panel"
-                direction="horizontal">
-    <el-aside width="200px"
-              class="subnav hidden-xs-only">
+  <el-container class="content panel" direction="horizontal">
+    <el-aside width="200px" class="subnav">
       <mo-task-subnav :current="status" />
     </el-aside>
-    <el-container class="content panel"
-                  direction="vertical">
-      <el-header class="panel-header"
-                 height="84">
-        <h4 class="task-title hidden-xs-only">{{ title }}</h4>
-        <mo-task-switcher :title="title"
-                          :commands="titles"
-                          class="hidden-sm-and-up" />
+    <el-container class="content panel" direction="vertical">
+      <el-header class="panel-header" height="84">
+        <h4>{{ title }}</h4>
         <mo-task-actions />
       </el-header>
       <el-main class="panel-content">
@@ -23,60 +16,48 @@
 </template>
 
 <script>
-import TaskSubnav from '@/components/Subnav/TaskSubnav'
-import TaskActions from '@/components/Task/TaskActions'
-import TaskList from '@/components/Task/TaskList'
-import TaskSwitch from '@/components/Task/TaskSwitcher'
+  import TaskSubnav from '@/components/Subnav/TaskSubnav'
+  import TaskActions from '@/components/Task/TaskActions'
+  import TaskList from '@/components/Task/TaskList'
 
-export default {
-  name: 'mo-content-task',
-  components: {
-    [TaskSubnav.name]: TaskSubnav,
-    [TaskActions.name]: TaskActions,
-    [TaskList.name]: TaskList,
-    [TaskSwitch.name]: TaskSwitch
-  },
-  props: {
-    status: {
-      type: String,
-      default: 'active'
-    }
-  },
-  computed: {
-    titleMap: function () {
-      return {
-        active: this.$t('task.active'),
-        waiting: this.$t('task.waiting'),
-        stopped: this.$t('task.stopped')
+  export default {
+    name: 'mo-content-task',
+    components: {
+      [TaskSubnav.name]: TaskSubnav,
+      [TaskActions.name]: TaskActions,
+      [TaskList.name]: TaskList
+    },
+    props: {
+      status: {
+        type: String,
+        default: 'active'
       }
     },
-    titles: function () {
-      return Object.keys(this.titleMap).map(key => {
-        return {
-          key,
-          label: this.titleMap[key]
+    computed: {
+      title: function () {
+        const titles = {
+          'active': this.$t('task.active'),
+          'waiting': this.$t('task.waiting'),
+          'stopped': this.$t('task.stopped')
         }
-      })
+        return titles[this.status]
+      }
     },
-    title: function () {
-      return this.titleMap[this.status]
-    }
-  },
-  watch: {
-    status: 'onStatusChange'
-  },
-  methods: {
-    onStatusChange () {
+    watch: {
+      'status': 'onStatusChange'
+    },
+    methods: {
+      onStatusChange () {
+        this.changeCurrentList()
+      },
+      changeCurrentList () {
+        this.$store.dispatch('task/changeCurrentList', this.status)
+      }
+    },
+    created: function () {
       this.changeCurrentList()
-    },
-    changeCurrentList () {
-      this.$store.dispatch('task/changeCurrentList', this.status)
     }
-  },
-  created: function () {
-    this.changeCurrentList()
   }
-}
 </script>
 
 <style lang="scss">
